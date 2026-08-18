e11a818f6fc7291ee659191e28bf5706a5ba3958
Ilinka Bibic <bibic.ilinka@gmail.com>
2026-08-18 23:09:54 +0200

    Add data annotations to Restaurant request DTOs

--- RestaurantService.Application/DTOs/UpdateRestaurantRequest.cs
+++ RestaurantService.Application/DTOs/UpdateRestaurantRequest.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.ComponentModel.DataAnnotations;
+
+
 namespace RestaurantService.Application.DTOs;
 
 public class UpdateRestaurantRequest
 {
+    [Required]
+    [StringLength(150, MinimumLength = 2)]
     public string NameSr { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(150, MinimumLength = 2)]
     public string NameEn { get; set; } = string.Empty;
+
+    [StringLength(1000)]
     public string DescriptionSr { get; set; } = string.Empty;
+
+    [StringLength(1000)]
     public string DescriptionEn { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(300)]
     public string Address { get; set; } = string.Empty;
+
+    [Url]
+    [StringLength(500)]
     public string ImageUrl { get; set; } = string.Empty;
+
     public bool IsActive { get; set; }
     public bool IsFeatured { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public string CuisineType { get; set; } = string.Empty;
 }
