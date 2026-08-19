4bc02ec7707c52fac9d3464a59881ceafd9aa1db
hodzicalma <hodzicalma8@gmail.com>
2026-08-19 23:49:53 +0200

    prelazak na .net 10 sa .net 8 i gRPC endpoint

--- delivery-service/Delivery.Api/Program.cs
+++ delivery-service/Delivery.Api/Program.cs
@@ -1,7 +1,15 @@
 using Delivery.Api.Data;
+using Delivery.Api.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080, listenOptions => listenOptions.Protocols = HttpProtocols.Http1);
+    options.ListenAnyIP(8081, listenOptions => listenOptions.Protocols = HttpProtocols.Http2);
+});
 
 // Add services to the container.
 
@@ -12,6 +20,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DeliveryDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DeliveryDb")));
+
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
@@ -27,5 +37,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<DeliveryGrpcServiceImpl>();
 
 app.Run();
