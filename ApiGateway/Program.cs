66b2643e2f0884731c916342da66fe81abb38a06
hodzicalma <hodzicalma8@gmail.com>
2026-08-22 21:59:39 +0200

    Dodata ruta za Delivery servis u API Gateway i cors za frontend

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -10,13 +10,25 @@ builder.Configuration
         reloadOnChange: true);
  
 builder.Services.AddOcelot(builder.Configuration);
- 
+
 builder.Services.AddHealthChecks();
- 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
- 
+
 app.UseHealthChecks("/health");
- 
+
+app.UseCors("AllowFrontend");
+
 await app.UseOcelot();
  
 app.Run();
